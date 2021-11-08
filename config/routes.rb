bdda65c16f10608df55902b85887a37f132279ba
xiangma0531 <shoma0531@outlook.com>
2021-11-08 22:01:42 +0900

    ルーティング2

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
     passwords: 'users/passwords',
     registrations: 'users/registrations'
   }
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :admins
+  resources :users
+  root to: 'users#index'
 end
