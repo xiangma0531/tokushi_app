10e8ae6378997632aba6cd731730ad0e0f64c9f0
xiangma0531 <shoma0531@outlook.com>
2021-11-09 15:02:01 +0900

    サインアップ・ビュー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,13 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters_user
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_path
+    when User
+      user_path
+    end
+  end
 end
